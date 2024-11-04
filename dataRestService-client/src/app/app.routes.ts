c6e0932c541eb7a072313074e82f4790188f6a79
Kuan-Yu Chang <grouper.chang@gmail.com>
2024-11-03 23:23:23 -0500

    final commit

--- dataRestService-client/src/app/app.routes.ts
+++ dataRestService-client/src/app/app.routes.ts
@@ -41,6 +41,7 @@ export const routes: Routes = [
 ];
 
 @NgModule({
+
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- dataRestService-server/build.gradle.kts
+++ dataRestService-server/build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-hateoas")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui")
 	runtimeOnly("com.mysql:mysql-connector-j")
